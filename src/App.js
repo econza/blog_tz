1c3dd18951edf9ecadd722e2acf3019eab23a8d9
zayats_vn <zayatsslava@gmail.com>
2022-04-06 19:23:38 +0300

    add ui compnents

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
-import Counter from "./components/Counter";
+import React, { useState } from "react";
+import PostForm from "./components/PostForm";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import "./styles/App.css";
 
 function App() {
-  // const [value, setValue] = useState("NTRC");
+  const [posts, setPosts] = useState([
+    { id: 1, title: "Javascript", body: "Description" },
+    { id: 2, title: "Javascript 2", body: "Description" },
+    { id: 2, title: "Javascript 3", body: "Description" },
+  ]);
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  };
 
   return (
     <div className="App">
-      <Counter />
+      <PostForm create={createPost} />
+      <PostList posts={posts} title={"Список постов 1"} />
     </div>
   );
 }
