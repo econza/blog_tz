a5bf9a88afb8ae9d46cea90169c4aca6addb9b38
zayats_vn <zayatsslava@gmail.com>
2022-04-22 12:15:56 +0300

    add a lot of ui, routers

--- src/App.js
+++ src/App.js
@@ -1,66 +1,34 @@
-import React, { useState, useEffect } from "react";
-import PostService from "./API/PostService";
-import PostFilter from "./components/PostFilter";
-import PostForm from "./components/PostForm";
-import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import Loader from "./components/UI/Loader/loader";
-import MyModal from "./components/UI/MyModal/MyModal";
-import { useFetching } from "./hooks/useFetching";
-import { usePosts } from "./hooks/usePosts";
+import React, { useEffect, useState } from "react";
 import "./styles/App.css";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/UI/Navbar/Navbar";
+import AppRouter from "./components/AppRouter";
+import { AuthContext } from "./context";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [filter, setFilter] = useState({ sort: "", query: "" });
-  const [modal, setModal] = useState(false);
-  const [totalCount, setTotalCount] = useState(0);
-  const [limit, setLimit] = useState(10);
-  const [page, setPage] = useState(1);
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    const response = await PostService.getAll(limit, page);
-    setPosts(response.data);
-    setTotalCount(response.headers["x-total-count"]);
-  }, []);
+  const [isAuth, setIsAuth] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+
   useEffect(() => {
-    fetchPosts();
+    if (localStorage.getItem("auth")) {
+      setIsAuth(true);
+    }
+    setLoading(false);
   }, []);
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost]);
-    setModal(false);
-  };
-
-  const removePost = (post) => {
-    setPosts(posts.filter((p) => p.id !== post.id));
-  };
 
   return (
-    <div className="App">
-      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
-        Создать пользователя
-      </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost} />
-      </MyModal>
-      <hr style={{ margin: "15px 0" }}></hr>
-      <PostFilter filter={filter} setFilter={setFilter} />
-      {postError && <h1>Произошла ошибка ${postError}</h1>}
-      {isPostsLoading ? (
-        <div
-          style={{ display: "flex", justifyContent: "center", marginTop: 50 }}
-        >
-          <Loader />
-        </div>
-      ) : (
-        <PostList
-          remove={removePost}
-          posts={sortedAndSearchedPosts}
-          title={"Список постов 1"}
-        />
-      )}
-    </div>
+    <AuthContext.Provider
+      value={{
+        isAuth,
+        setIsAuth,
+        isLoading,
+      }}
+    >
+      <BrowserRouter>
+        <Navbar />
+        <AppRouter />
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
